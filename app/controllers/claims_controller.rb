ca6ee784edad909ae5d4bd79cebe3420c14ce3e6
Christian Bermeo <czaudiovisual@hotmail.com>
2021-10-27 23:36:28 -0400

    adding validations to user

--- app/controllers/claims_controller.rb
+++ app/controllers/claims_controller.rb
@@ -1,2 +1,7 @@
 class ClaimsController < ApplicationController
+
+    def index
+        claim = Claim.all
+        render json: claim, status: :ok
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ApplicationRecord
     has_many :claims
     # attr_accessor :name, :username, :profile_picture, :password, :password_confirmation
+
+    # validation 
+    validates :name, presence: true
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :password, presence: true, case_sensitive: false
+    
+
     has_secure_password
 end
